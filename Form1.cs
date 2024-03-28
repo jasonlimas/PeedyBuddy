1ccb6b830417d541fce6e1aa383de098b070c68d
jasonlimas <jasonandrea14@gmail.com>
2024-03-29 03:21:26 +0700

    Add speech recog. with System.Speech.Recognition

--- Form1.cs
+++ Form1.cs
@@ -7,38 +7,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Diagnostics;
+using static PeedyBuddy.StaticInfo;
 
 namespace PeedyBuddy
 {
     public partial class FormDashboard : Form
     {
+        // Static field to store a reference to the current instance of FormDashboard
+        private static FormDashboard currentInstance;
+
         DoubleAgent.AxControl.AxControl newAgent;
+        SpeechRecognitionEngine speechRecognizer;
 
         public FormDashboard()
         {
             InitializeComponent();
+
+            // Set the currentInstance to this instance of FormDashboard
+            currentInstance = this;
         }
 
         private void FormDashboard_Load(object sender, EventArgs e)
         {
             newAgent = new DoubleAgent.AxControl.AxControl();
             newAgent.CreateControl();
-            newAgent.Characters.Load("Peedy", "E:\\Others\\Peedy.acs");
+            newAgent.Characters.Load(StaticInfo.charName, StaticInfo.charFileLocation);
+            loadSpeechRecognition();
+            newAgent.Characters[charName].Show();
         }
 
         private void ButtonShow_Click(object sender, EventArgs e)
         {
-            newAgent.Characters["Peedy"].Show();
+            newAgent.Characters[StaticInfo.charName].Show();
         }
 
         private void ButtonHide_Click(object sender, EventArgs e)
         {
-            newAgent.Characters["Peedy"].Hide();
+            newAgent.Characters[StaticInfo.charName].Hide();
         }
 
         private void ButtonSpeak_Click(object sender, EventArgs e)
         {
-            newAgent.Characters["Peedy"].Speak(TextSpeak.Text);
+            newAgent.Characters[StaticInfo.charName].Speak(TextSpeak.Text);
         }
+
+        private void loadSpeechRecognition()
+        {
+            // Initialize speech recognizer with en-US language/locale
+            speechRecognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+
+            // Create grammar
+            GrammarBuilder grammarBuilder = new GrammarBuilder(getChoiceLibrary());
+            Grammar grammar = new Grammar(grammarBuilder);
+            speechRecognizer.LoadGrammar(grammar);
+
+            // Add a handler for the speech recognition event
+            speechRecognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(speechRecognizer_SpeechRecognized);
+
+            // Configure the input to the speech recognizer.
+            speechRecognizer.SetInputToDefaultAudioDevice();
+
+            // Start asynchronous, continuous speech recognition.
+            speechRecognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private Choices getChoiceLibrary()
+        {
+            Choices choice = new Choices();
+
+            // Greetings
+            choice.Add(new string[]
+            {
+                "hey " + StaticInfo.charName,
+                "hi " + StaticInfo.charName,
+                "hello " + StaticInfo.charName
+            });
+
+            // Open things
+            choice.Add(new string[]
+            {
+                StaticInfo.charName + " open youtube",
+                StaticInfo.charName + " open steam",
+                StaticInfo.charName + " open notepad"
+            });
+
+            return choice;
+        }
+
+        // Handle the SpeechRecognized event.
+        static void speechRecognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Access the newAgent field through the currentInstance
+            DoubleAgent.AxControl.AxControl newAgent = currentInstance.newAgent;
+
+            switch (e.Result.Text)
+            {
+                case "hey " + StaticInfo.charName:
+                case "hi " + StaticInfo.charName:
+                case "hello " + StaticInfo.charName:
+                    newAgent.Characters[StaticInfo.charName].Speak("Well hello there! I hope you're doing fine today.");
+                    break;
+                case StaticInfo.charName + " open notepad":
+                    Process.Start("notepad.exe", "");
+                    // newAgent.Characters[StaticInfo.charName].Play("Wave"); TODO: Add animation later
+                    break;
+                case charName + " open steam":
+                    Process.Start("steam.exe", "");
+                    break;
+                case charName + " open youtube":
+                    Process.Start("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe", "https://youtube.com");
+                    break;
+            }
+        }
+
     }
 }
---
+++ StaticInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeedyBuddy
+{
+    public static class StaticInfo
+    {
+        public const string charName = "Peedy";
+        public const string charFileLocation = "E:\\Others\\Peedy.acs";
+    }
+}
